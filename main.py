7abd4dece48c4aa86d2a553c312d3675913b67dc
Filip Radzikowski <fradzi1@uic.edu>
2016-03-30 09:04:01 -0500

    Add extract_entity_name function.

--- main.py
+++ main.py
@@ -23,11 +23,28 @@ def tagNameEntity(sentences):
 		for sentence in sentences:
 		    tokens = nltk.word_tokenize(sentence)
 		    tagged = nltk.pos_tag(tokens)
-		    # nltk.ne_chuck() is a classifier that has already been trained to recognize named entities 
+		    # nltk.ne_chuck() is a classifier that has already 
+            # been trained to recognize named entities 
 		    entities = nltk.chunk.ne_chunk(tagged)
 		    print entities
 	except Exception, e:
 		print str(e)
+
+
+def extract_entity_names(t):
+    entity_names = []
+
+    if hasattr(t, 'label') and t.label:
+        if t.label() == 'NE':
+            entity_names.append(' '.join([child[0] for child in t]))
+        else:
+            for child in t:
+                entity_names.extend(extract_entity_names(child))
+
+    return entity_names
+
+
+
 
 
 if __name__ == "__main__":
