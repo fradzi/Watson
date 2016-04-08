9b4aaefb4f9489b1a01cd523257431fb78b89708
Revanth Reddy <revanthreddy1@gmail.com>
2016-04-08 15:28:23 -0500

    Merge stanford parser and begin category logic
    - stanford parser and classpath details added to top of main.py
    - add getCategories() dictionary files

--- main.py
+++ main.py
@@ -5,22 +5,47 @@ Watson Jr.
 '''
 import nltk
 import sqlite3
+from domainTags import *
+
+import os
+from nltk.parse import stanford
+
+# For user to change
+# USERSTANFORDDIR = '/Users/fr/Downloads'  # for Filip
+USERSTANFORDDIR = '/Users/revanthreddy/Downloads'  # for Rev
+
+
+STANFORDTOOLSDIR = USERSTANFORDDIR + '/stanford-parser'
+os.environ['STANFORD_PARSER'] = STANFORDTOOLSDIR+'/stanford-parser.jar'
+os.environ['STANFORD_MODELS'] = STANFORDTOOLSDIR+'/stanford-parser-3.3.0-models.jar'
+os.environ['CLASSPATH'] = STANFORDTOOLSDIR+'/stanford-parser.jar:' + STANFORDTOOLSDIR + '/stanford-parser-3.3.0-models.jar'
 
 
 
 def main():
+	# Extract the stanford-parser-3.3.0-models.jar file in order to set the model_path
+    parser = stanford.StanfordParser(model_path= USERSTANFORDDIR + "/stanford-parser/stanford-parser-3.3.0-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+    sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your name?"))
+
+    # Draw tree graphic
+    for line in sentences: 
+        for sentence in line:
+            sentence.draw()
+
+
 	# Get sentences from input file
-    inputs = parseLines()
+    # inputs = parseLines()
     
     # Pass the sentences through a POS tagger
-    taggedSentences = getPOSTags(inputs)
+    # taggedSentences = getPOSTags(inputs)
 
     # Extract NE from sentences and contain into list of tuples
-    entities = extract_entities(taggedSentences)
+    # entities = getNameEntities(taggedSentences)
 
     # Determine what catagory each sentence is in
     # getSentenceCategory(entities);
-
+    # getCategories(taggedSentences, entities)
+	
 
 def parseLines():
     with open('sample_sentences.txt', 'r') as f:
@@ -35,7 +60,7 @@ def getPOSTags(sentences):
 	return taggedSentences
 
 
-def extract_entities(sentences):
+def getNameEntities(sentences):
 	alltuples = []
 	for s in sentences:
 		t = ()
@@ -52,22 +77,42 @@ def extract_entities(sentences):
 	return alltuples
 
 
-def getSentenceCategory(entities):
-	count=0
-	for tpl in entities:
-		count+=1
-		print '**********Sentence %s' % count
-		print tpl
-		if len(tpl) == 0:
-			print '*empty*'
-			continue
-		for word in tpl:
-			if word[0] == 'GPE':
-				print 'Geographic'
-			elif word[0] == 'ORGANIZATION':
-				print 'Organization'
-			elif word[0] == 'PERSON':
-				print 'Person'
+def getCategories(tagged, entities):
+	NVs = {
+		'NN':True, 
+		'NNP':True, 
+		'NNPS':True, 
+		'NNS':True, 
+		'VB':True,
+		'VBD':True,
+		'VBG':True,
+		'VBN':True,
+		'VBP':True,
+		'VBZ':True,
+		}
+
+	for sentence in tagged:
+		# For each word in sentence, save only nouns and verbs in new list
+		NounsAndVerbs = [tpl for tpl in sentence if NVs.get(tpl[1])]
+		print NounsAndVerbs
+		
+
+# def getSentenceCategory(entities):
+# 	count=0
+# 	for tpl in entities:
+# 		count+=1
+# 		print '**********Sentence %s' % count
+# 		print tpl
+# 		if len(tpl) == 0:
+# 			print '*empty*'
+# 			continue
+# 		for word in tpl:
+# 			if word[0] == 'GPE':
+# 				print 'Geographic'
+# 			elif word[0] == 'ORGANIZATION':
+# 				print 'Organization'
+# 			elif word[0] == 'PERSON':
+# 				print 'Person'
 
 
 if __name__ == "__main__":
